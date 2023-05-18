a62cd354da5de18078525da772755ae26181e0f1
valdir.junior90 <valdir.junior@alunos.ufersa.edu.br>
2023-05-17 22:16:41 -0300

    --

--- UserCliente.java
+++ UserCliente.java
@@ -2,7 +2,6 @@ public class UserCliente extends User {
     private static String tipo = "Cliente";
     public UserCliente(String nome, String login, String senha) {
         super(nome, login, senha, tipo);
-        
     }
     
 }
